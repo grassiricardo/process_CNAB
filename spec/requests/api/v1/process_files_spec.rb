f2c8a6759c99fb8ce3a102e98c3705346c316bee
Ricardo Grassi <grassiricardorg@gmail.com>
2018-03-03 17:47:25 -0300

    corrections

--- spec/requests/api/v1/process_files_spec.rb
+++ spec/requests/api/v1/process_files_spec.rb
@@ -38,5 +38,24 @@ RSpec.describe 'Request Process Files', type: :request do
         expect(JSON.parse(response.body).count).to eq(2)
       end
     end
+
+    context 'with param type_transaction' do
+      let!(:process_file_credito) { create(:process_file, type_transaction: 1) }
+      let!(:process_file_debito) { create(:process_file, type_transaction: 4) }
+
+      before do
+        user = create(:user)
+        auth_headers = user.create_new_auth_token
+        get '/api/v1/transactions', params: { type_transaction: 1 }, headers: auth_headers
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the transactions createds' do
+        expect(JSON.parse(response.body).count).to eq(1)
+      end
+    end
   end
 end
